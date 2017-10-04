373de129b9f8e2470eb4174967b33787baccb561
Bolimbo <carlo.cappellini@yahoo.com>
2017-10-04 15:36:06 -0500

    view contacts and add contacts done

--- src/contacts_manager/ContentsTest.java
+++ src/contacts_manager/ContentsTest.java
@@ -6,13 +6,35 @@ import java.nio.file.Paths;
 
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
-public class ContentsTest {
+public class ContentsTest  {
+
     public static void main(String[] args) throws IOException {
-        String directory = "data";
-        String filename = "contacts.txt";
+        Scanner scan = new Scanner(System.in);
 
-        FileHandler contactsFile = new FileHandler (directory, filename);
-        contactsFile.createFile();
+        FileHandler filehandler = new FileHandler("data", "contacts.txt");
+
+        List<String> contact = new ArrayList<>();
+        contact.add("carlo | 123-123-1234");
+        contact.add("brandon | 234-234-2345");
+
+        System.out.println("ad a cobatct");
+        String add = scan.nextLine();
+        contact.add(add);
+
+        filehandler.writeToFile(contact); // write several lines to a file
+        printAllContacts(filehandler);
+
+    }
+
+    public static void printAllContacts(FileHandler fh) throws IOException {
+        List<String> contacts = fh.readFromFile();
+
+        for (String contact : contacts){
+            System.out.println(contact);
+        }
     }
 }
